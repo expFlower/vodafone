824e23a73ba427d53784a8485626ce60495ea71f
David Fowler <dafowler@homeaway.com>
2019-05-22 23:41:24 +0100

    initial commit

---
+++ exercise1/README.md
@@ -0,0 +1,70 @@
+
+# Install Wordpress in Docker with Nginx and MariaDB
+
+## Start-up
+
+To get started, after you clone this repo, run `docker-compose up -d` and then visit http://localhost/
+
+Please note that we are using docker-compose file format 3.7, please ensure that your docker engine is at 18.06.0+ 
+
+Please see [docker-compose file versions](https://docs.docker.com/compose/compose-file/compose-versioning/) for more details
+
+
+### Design notes:
+
+
+####Wordpress:
+
+[Official docker wordpress:alpine image](https://docs.docker.com/samples/library/wordpress/) with all defaults.
+This is alpine image of wordpress, used to keep the image size as small as possible
+This image automatically populates the wp-config.php configuration. 
+
+This is a base setup, if you indeed to run any live site using this setup you are **STRONGLY** advised to follow and 
+implement [wordpress-hardening](https://wordpress.org/support/article/hardening-wordpress/)
+
+I'd also strong recommending tuning PHP-FPM settings depending on your workload. 
+
+
+####Nginx:
+
+[Official docker nginx:alpine image.](https://docs.docker.com/samples/library/nginx/) This is alpine image of wordpress,
+ used to keep the image size as small as possible.
+ 
+Nginx is configured with the following:
+
+* Micro caching
+* HTTPS enabled
+* Redirect HTTP to HTTPS
+* TLS only. Strong cyphers. 
+See [Mozilla - ssl-config-generator](https://mozilla.github.io/server-side-tls/ssl-config-generator/)
+* dhparam. 
+See [Mozilla - Server_Side_TLS#DHE_handshake_and_dhparam](https://wiki.mozilla.org/Security/Server_Side_TLS#DHE_handshake_and_dhparam)
+* OCSP configured but we only have self sign certs it is currently ignored.  
+See [Mozilla - Server_Side_TLS#OCSP_Stapling](https://wiki.mozilla.org/Security/Server_Side_TLS#OCSP_Stapling) 
+
+* HSTS enabled. See [Mozilla -Server_Side_TLS#HSTS:_HTTP_Strict_Transport_Security](https://wiki.mozilla.org/Security/Server_Side_TLS#HSTS:_HTTP_Strict_Transport_Security)
+* Numerous HTTP header settings including:
+
+  *`Strict-Transport-Security`HSTS (ngx_http_headers_module is required) (15768000 seconds = 6 months)*
+  
+  *`X-Content-Type-Options nosniff;` Prevents browsers from trying to "guess" MIME types and such, forcing them to use what the server tells them.*
+   
+  *`X-Frame-Options SAMEORIGIN;`Stops your site from being included in iframes on other sites*
+ 
+  *`X-Xss-Protection "1" always;`Activates cross-scripting (XSS) protection in browsers.*
+  
+  *`Referrer-Policy same-origin;`Makes the site always send referrer information to other sites.*
+  
+  *`Content-Security-Policy "default-src 'self';";` Content to come from the site's own origin, trusted domains can be added if needed.*
+
+
+A self-signed certificate was generated using the following:
+
+`openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx-selfsigned.key -out ./nginx-selfsigned.crt -subj "/C=GB/ST=London/L=London/O=DemoCorp/OU=Org/CN=localhost"`
+
+
+The dhparam key was generate using the following:
+
+`openssl dhparam -out /etc/nginx/dhparam.pem 4096`
+
+
---
+++ exercise1/wordpress/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'c*aBec4zzhMTqydG');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mariaDB:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '898ddb9340460f75f71e6b4c1524d251dcf4fb68');
+define( 'SECURE_AUTH_KEY',  '67af40f45a60995df873e09616cfe8862e58755e');
+define( 'LOGGED_IN_KEY',    'cb066189144a7df66d135cb79b767a35993bc21e');
+define( 'NONCE_KEY',        '1cd3c70739dd88e79b54352fdd98bd4b49e4fab5');
+define( 'AUTH_SALT',        '03298e8a2a4d136a5b873a5c049ebfe0ee1cdfc4');
+define( 'SECURE_AUTH_SALT', 'dbdafa0167e9b919e70171b42e12632c684c27cb');
+define( 'LOGGED_IN_SALT',   '94c6dd35073fa46ba3fec938c89d34dc2d98bfec');
+define( 'NONCE_SALT',       'c2fc69a2b50ec0912b0cfd83a2af61a633be20b7');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
